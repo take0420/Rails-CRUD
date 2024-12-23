499cdbf7c4294f633abf953db94d3d6f22582b59
Takehiro_Yamazaki <111678214+take0420@users.noreply.github.com>
2024-12-23 00:37:31 +0000

    feat:

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_19_054326) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_23_003636) do
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -18,4 +26,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_19_054326) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
 end
