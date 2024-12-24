00fdb2c4837544e189bb126a532825127a553987
Takehiro_Yamazaki <111678214+take0420@users.noreply.github.com>
2024-12-24 03:46:08 +0000

    feat:

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,10 +4,16 @@ class CommentsController < ApplicationController
     @comment = @post.comments.build(comment_params)
 
     if @comment.save
-      redirect_to post_path(:post_id)
+      redirect_to @post
     else
       render 'posts/show', status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @post.comments.find(params[:id]).destroy
+    redirect_to @post
   end
 
   private
